b9d76d5eb737714794304a5e964e74506b8449a6
RieNah <rinahratsimilo@gmail.com>
2024-11-06 18:44:36 +0100

    onClick navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo/logo-header.png'
+
 const Header = () => {
+   //On utilise useState pour gérer l'état des liens au clic
+   const [activeLink, setActiveLink] = useState(null)
+    function handleClick(link){
+        setActiveLink(link)
+    }
     return (
         <header>
             <img src={logo} alt="logo" />
             <nav>
-                <Link to='/'>Accueil</Link>
-                <Link to='/about' >A propos</Link>
+                <Link to='/' onClick={()=>handleClick("home")} className={activeLink === "home" ? "nav-active" : null}>
+                    Accueil
+                </Link>
+                <Link to='/about' onClick={()=>handleClick("about")} className={activeLink === "about" ? "nav-active" : null}>
+                    A propos
+                </Link>
             </nav>
         </header>
     );
